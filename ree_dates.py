864d953f369f0ffcde52c15ef7ef915c5127a7c5
Jose Luis - acerE5 <jlrandmore.me@gmail.com>
2021-08-25 13:07:53 +0200

    ree periods included

--- ree_dates.py
+++ ree_dates.py
@@ -1,17 +1,37 @@
 import calendar
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 import holidays
 import pandas as pd
 import numpy as np
+import re
 
 class reeHolidays(holidays.Spain):
      def _populate(self, year):
-         # Populate the holiday list with the default ES holidays
-         holidays.Spain._populate(self, year)
-         # Remove Wrong Festivos
-         self.pop_named("Epifanía del Señor")
-         self.pop_named("Asunción de la Virgen (Trasladado)")
-         #self.pop_named("Asunción de la Virgen")
+        # Populate the holiday list with the default ES holidays
+        full_holidays =  holidays.Spain._populate(self, year)
+        # Remove Festivos Trasladados. That doesn't apply to REE
+        trasladados = []
+        for day in self.values():
+            if re.search("(Trasladado)", day):
+                trasladados.append(day)
+            
+        for item in trasladados:
+            self.pop_named(item)
+        
+        # Then remove other 2 ones 
+        # try:
+        #     self.pop_named("Epifanía del Señor")
+        # except KeyError:
+        #     print("No existe Epifania")
+        
+        try:
+            self.pop_named("Viernes Santo")
+        except KeyError:
+            self.pop_named('No existe Viernes Santo')
+        
+        # Add  Festivos (Just to remember)
+        #self[ date( year , 1,6)] = "Día de Reyes"
+
 
 def ree_periods( startdate, enddate ):
     """ 
@@ -21,50 +41,68 @@ def ree_periods( startdate, enddate ):
     assert type(startdate) == datetime, 'Must be datetime'
     assert type(enddate) == datetime, 'Must be datetime'
 
-    # Functions
+    # Work with dates
     dates = ree_dates(startdate, enddate)
-    dates["period"] = 'P0' # valor por defecto
+    
+    # Periodos 2.0TD
+    dates["20TD_periods"] = 'P0' # valor por defecto
+    
+    fest = (dates["feriado_finde"] == 1).values
+    energy_20TD_P1 = dates.index.hour.isin( [10,11,12,13,18,19,20,21] ) & (fest == False) 
+    energy_20TD_P2 = dates.index.hour.isin( [8,9,14,15,16,17,22,23] ) & (fest == False) 
+    energy_20TD_P3 = dates.index.hour.isin( [0,1,2,3,4,5,6,7,] ) & (fest == False) 
+    
+    potence_20TD_P1 = dates.index.hour.isin( range(0,8) )
+    potence_20TD_P2 = dates.index.hour.isin( range(8,24) )
 
-    # Periodo 6.1A
-    ene_61TD_P6 = (dates["datetime"].dt.month == 1 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    ene_61TD_P2 = (dates["datetime"].dt.month == 1 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    ene_61TD_P1 = (dates["datetime"].dt.month == 1 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    feb_61TD_P6 = (dates["datetime"].dt.month == 2 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    feb_61TD_P2 = (dates["datetime"].dt.month == 2 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    feb_61TD_P1 = (dates["datetime"].dt.month == 2 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    mar_61TD_P6 = (dates["datetime"].dt.month == 3 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    mar_61TD_P3 = (dates["datetime"].dt.month == 3 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    mar_61TD_P2 = (dates["datetime"].dt.month == 3 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    abr_61TD_P6 = (dates["datetime"].dt.month == 4 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    abr_61TD_P5 = (dates["datetime"].dt.month == 4 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    abr_61TD_P4 = (dates["datetime"].dt.month == 4 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    may_61TD_P6 = (dates["datetime"].dt.month == 5 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    may_61TD_P5 = (dates["datetime"].dt.month == 5 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    may_61TD_P4 = (dates["datetime"].dt.month == 5 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    jun_61TD_P6 = (dates["datetime"].dt.month == 6 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    jun_61TD_P4 = (dates["datetime"].dt.month == 6 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    jun_61TD_P3 = (dates["datetime"].dt.month == 6 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    jul_61TD_P6 = (dates["datetime"].dt.month == 7 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    jul_61TD_P2 = (dates["datetime"].dt.month == 7 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    jul_61TD_P1 = (dates["datetime"].dt.month == 7 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    ago_61TD_P6 = (dates["datetime"].dt.month == 8 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    ago_61TD_P4 = (dates["datetime"].dt.month == 8 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    ago_61TD_P3 = (dates["datetime"].dt.month == 8 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    sep_61TD_P6 = (dates["datetime"].dt.month == 9 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    sep_61TD_P4 = (dates["datetime"].dt.month == 9 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    sep_61TD_P3 = (dates["datetime"].dt.month == 9 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    oct_61TD_P6 = (dates["datetime"].dt.month == 10 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    oct_61TD_P5 = (dates["datetime"].dt.month == 10 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    oct_61TD_P4 = (dates["datetime"].dt.month == 10 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    nov_61TD_P6 = (dates["datetime"].dt.month == 11 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    nov_61TD_P3 = (dates["datetime"].dt.month == 11 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    nov_61TD_P2 = (dates["datetime"].dt.month == 11 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    dic_61TD_P6 = (dates["datetime"].dt.month == 12 ) & (dates["datetime"].dt.hour.isin( [0,1,2,3,4,5,6,7]) )
-    dic_61TD_P2 = (dates["datetime"].dt.month == 12 ) & (dates["datetime"].dt.hour.isin( [8,14,15,16,17,22,23]) )
-    dic_61TD_P1 = (dates["datetime"].dt.month == 12 ) & (dates["datetime"].dt.hour.isin( [9,10,11,12,13,18,19,20,21]) )
-    fest = dates["feriado_finde"]
+    dates['20TD_periods'] = np.select(
+        [energy_20TD_P1, energy_20TD_P2, energy_20TD_P3, fest],
+        [1,2,3,3]
+    )
+    
+    # Periodo 6.1TD y 3.0TD
+    dates["61TD_periods"] = 'P0' # valor por defecto
+    dates["30TD_periods"] = 'P0'
 
-    dates['period'] = np.select([
+    fest = (dates["feriado_finde"] == 1).values
+    ene_61TD_P6 = (dates.index.month == 1 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    ene_61TD_P2 = (dates.index.month == 1 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    ene_61TD_P1 = (dates.index.month == 1 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    feb_61TD_P6 = (dates.index.month == 2 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    feb_61TD_P2 = (dates.index.month == 2 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    feb_61TD_P1 = (dates.index.month == 2 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    mar_61TD_P6 = (dates.index.month == 3 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    mar_61TD_P3 = (dates.index.month == 3 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    mar_61TD_P2 = (dates.index.month == 3 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    abr_61TD_P6 = (dates.index.month == 4 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    abr_61TD_P5 = (dates.index.month == 4 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    abr_61TD_P4 = (dates.index.month == 4 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    may_61TD_P6 = (dates.index.month == 5 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    may_61TD_P5 = (dates.index.month == 5 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    may_61TD_P4 = (dates.index.month == 5 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    jun_61TD_P6 = (dates.index.month == 6 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    jun_61TD_P4 = (dates.index.month == 6 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    jun_61TD_P3 = (dates.index.month == 6 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    jul_61TD_P6 = (dates.index.month == 7 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    jul_61TD_P2 = (dates.index.month == 7 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    jul_61TD_P1 = (dates.index.month == 7 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    ago_61TD_P6 = (dates.index.month == 8 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    ago_61TD_P4 = (dates.index.month == 8 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    ago_61TD_P3 = (dates.index.month == 8 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    sep_61TD_P6 = (dates.index.month == 9 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    sep_61TD_P4 = (dates.index.month == 9 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    sep_61TD_P3 = (dates.index.month == 9 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    oct_61TD_P6 = (dates.index.month == 10 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    oct_61TD_P5 = (dates.index.month == 10 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    oct_61TD_P4 = (dates.index.month == 10 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    nov_61TD_P6 = (dates.index.month == 11 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    nov_61TD_P3 = (dates.index.month == 11 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    nov_61TD_P2 = (dates.index.month == 11 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+    dic_61TD_P6 = (dates.index.month == 12 ) & (dates.index.hour.isin( [0,1,2,3,4,5,6,7]) ) & (fest == False)
+    dic_61TD_P2 = (dates.index.month == 12 ) & (dates.index.hour.isin( [8,14,15,16,17,22,23]) ) & (fest == False)
+    dic_61TD_P1 = (dates.index.month == 12 ) & (dates.index.hour.isin( [9,10,11,12,13,18,19,20,21]) ) & (fest == False)
+
+    dates['61TD_periods'] = np.select([
         ene_61TD_P6, ene_61TD_P2, ene_61TD_P1,feb_61TD_P6, feb_61TD_P2, feb_61TD_P1,
         mar_61TD_P6, mar_61TD_P3, mar_61TD_P2,abr_61TD_P6, abr_61TD_P5, abr_61TD_P4,
         may_61TD_P6, may_61TD_P5, may_61TD_P4,jun_61TD_P6, jun_61TD_P4, jun_61TD_P3,
@@ -81,6 +119,10 @@ def ree_periods( startdate, enddate ):
             6]
     )
 
+    dates["30TD_periods"] = dates["61TD_periods"]
+
+    return dates 
+
 
 def ree_dates( startdate, enddate):
     """
@@ -89,8 +131,8 @@ def ree_dates( startdate, enddate):
     # Initial Assertions
     assert type(startdate) == datetime, 'Must be datetime'
     assert type(enddate) == datetime, 'Must be datetime'
-    # Functions
-    hours = []
+    
+    hours = [] # Outpu variable
 
     if startdate.year == enddate.year:
         year_spawn = range(startdate.year,enddate.year+1)
@@ -100,26 +142,30 @@ def ree_dates( startdate, enddate):
     # Calculate 'Festivos nacionales no desplazables'
     festivos = []
     for date in reeHolidays( prov=None, years = year_spawn).items():
-        for hour in range(0,13):
-            dt = datetime.combine(date[0], datetime.hour.time( hour ))
-            festivos.append( dt ) # Tengo el problema que me toma el 6 de enero y 16 de agosto como festivos nacionales. Eso no es así
-    print( len( festivos ))
+        dt = datetime.combine(date[0], datetime.min.time())
+        for hour in range(0,24):
+            festivos.append(dt + timedelta(hours = hour))
     
+    # Save, daylight saving days
+    daylight_saving = []
+
+    # Main Function
     for year in year_spawn:
         #print(year)
         for month in range(1,13): #
             #print(month)
-            c = calendar.monthcalendar(year, month) # month_weeks
+            c = calendar.monthcalendar(year, month) # month-weeks
             month_days = []
             for week in c:
                 for day in week:
                     if day != 0:
                         month_days.append(day)
 
+            # Detecting daylight saving time day
             last_week = c[-1]
             before_last_week = c[-2]
 
-            if month == 3: # march
+            if month == 3: # March: month were change from winter to summer occurs
                 if last_week[calendar.SUNDAY]:
                     day_short = last_week[calendar.SUNDAY]
                 else:
@@ -129,11 +175,11 @@ def ree_dates( startdate, enddate):
                     if day == day_short:
                         for hour in [0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]:
                             hours.append( datetime(year, month, day, hour) )
-                    else: # another day in march
+                    else: # any other in march
                         for hour in range(0,24):
                             hours.append( datetime(year, month, day, hour))
 
-            elif month == 10: # october
+            elif month == 10: # October: month were change from summer to winter occurs
                 if last_week[calendar.SUNDAY]:
                     day_long = last_week[calendar.SUNDAY]
                 else:
@@ -143,43 +189,49 @@ def ree_dates( startdate, enddate):
                     if day == day_long:
                         for hour in [0,1,2,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]:
                             hours.append( datetime(year, month, day, hour) )
-                    else: # another day in october
+                    else: # any other day in october
                         for hour in range(0,24):
                             hours.append( datetime(year, month, day, hour))
 
             else: 
                 for day in month_days:
-                    for hour in range(0,24): # usual days with 24 hours
+                    for hour in range(0,24): # normal days with 24 hours
                         hours.append( datetime(year, month, day, hour))
+
+            #daylight_saving.append([day_short, day_long])
 
     output = []
     for item in hours:
-        output.append( [
-            item.year,
-            item.month,
-            item.day,
-            item.hour,
-            item,
-            item.strftime("%Y/%m/%d %H:%M")])
+            output.append( [
+                item.year,
+                item.month,
+                item.day,
+                item.hour,
+                item,
+                item.strftime("%Y-%m-%dT%H:%M")])
 
     df = pd.DataFrame( output,  columns=["year", "month", "day", "hour", "datetime", "string"]) # add columns
+    df = df.set_index('datetime')
 
     # Set a flag for holidays and weekends (important for electric periods)
     df['feriado_finde'] = 0
-    df.loc[ ( df["datetime"].dt.weekday.isin([5,6]) | df["datetime"].isin(festivos) ) , 'feriado_finde' ] = 1
+    df.loc[ ( df.index.weekday.isin([5,6]) | df.index.isin(festivos) ) , 'feriado_finde' ] = 1
 
     # invierno_verano
-
-
+    # df['verano(1)/invierno(0)']
+    # for pair in daylight_saving:
+    #     summer_start = pair[0].strftime('%Y-%m-%d')
+    #     summer_ends = pair[1].strftime('%Y-%m-%d')
+    #     df.between_time( summer_start, summer_ends )
 
     # Before returning the output. Filter between the date: 
-    df = df.loc[ (df['datetime']>=startdate) & (df['datetime']<=enddate) ]
+    df = df.loc[ startdate : enddate ]
 
     return df
 
 if __name__ == '__main__':
     
-    startdate = datetime(2021, 1,1,10)
+    startdate = datetime(2019, 1,1,10)
     enddate = datetime(2022,5,1,8)
     ree_periods( startdate, enddate)
     print("END")
---
+++ scrapping.py
@@ -0,0 +1,7 @@
+"""
+    Scraping.py.
+    This directory have the functions needed to bring data for a whole month from ESIOS portal. 
+    The idea is to consolidate CSV and make them available in data folder
+"""
+
+# %%
